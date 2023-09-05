0a4b7f65d9d0e12eb9723b1b2c91fe0fd525608c
victor hernandez <victorherdev@gmail.com>
2023-09-05 09:54:04 -0300

    se añade el fichero strings

--- operadores.py
+++ operadores.py
@@ -29,7 +29,14 @@ print(3 != 4 )
 
 print("operadores logicos")
 #operadores logicos 
-
+#la logica de bool es por la que se rigen todos los lenguajes de programacion y la programacioncomo tal
+# falso y falso es falso
+# falso y verdadero es falso (viceversa)
+# verdadero y verdadero es verdadero 
+# falso o falso es falso
+# verdadero o falso es verdadero (viceversa)
+# veradero o verdadero es verdadero
+# recordar que not es un operador que niega lo anterior 
 print(3>4 and 4>5)
 print(3<4 and 4<5)
 print(3>4 or 4>5)
---
+++ strings.py
@@ -0,0 +1,17 @@
+'''STRINGS'''
+#los strings son cadenas de caracteres esto incluye letras numeros y caracteres !"#$%&/()==*"
+#en pyton los podemos definir por comillas simples y dobles 
+#definimos strings con variables asi es mas facil de llamarlas dentro de nuestro programa
+er_str = "este es mi string"
+do__str = "este es mi segundo string"
+
+print(er_str)
+#podemos utilizar operadores como vimos en operadores.py line 10
+print(er_str + " " + do__str)
+#existen strings con caracteres especiales que provocan ciertas funciones
+#definamos un salto de linea entre el string
+str_con_salto = "este es un string\ncon salto de linea"
+print(str_con_salto)
+#definamos un tab en nuestro string
+str_con_tab = "\teste es un string con tabulacion" 
+print(str_con_tab)
