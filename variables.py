400a2fd6611eea033457bc7d2948121625eefc75
victor hernandez <victorherdev@gmail.com>
2023-09-05 10:07:13 -0300

    se actualiza el string.py se añade el variables.py

---
+++ variables.py
@@ -0,0 +1,13 @@
+###VARIABLES###
+#las variables nos sirven para poder definir distintos tipos de datos para luego llamarlos en diferentes casos
+#variables de tipo string
+
+str_var = "soy una variable de tipo string"
+print(str_var)
+
+int_var = 101100
+print(int_var)
+
+int_to_str = str(int_var)
+print(int_to_str)
+print(type(int_to_str))
