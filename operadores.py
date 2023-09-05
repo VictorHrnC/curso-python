b6180eadb7cb2bc91356de0edf13e16bf775e171
victor hernandez <victorherdev@gmail.com>
2023-09-05 09:17:39 -0300

    se agrega un fichero de operadores

---
+++ operadores.py
@@ -0,0 +1,38 @@
+print("operadores aritmeticos")
+#operadores de aritmetica
+print(1+2)
+print(4-2)
+print(2*3)
+print(2/3)
+print(2**3)
+print(6%3)
+
+print("concatenando strings")
+#concatenando strings 
+print("hola " + "python")
+print("hola" + str(5))
+
+#multiplicar un strig con una operacion en float convirtiendolo en entero
+my_num = 2.5 * 2 
+print("hola" * int(my_num))
+
+print("operadores comparativos")
+#operadores comparativos
+print(3 < 4 )
+print(3 > 4 )
+print(5 == 4 )
+print(3 <= 4 )
+print(3 >= 4 )
+print(3 != 4 )
+
+#si queremos ver el orden alfabetico de una cadena de strings podemos utilizar los operadores comparativos en strings
+
+print("operadores logicos")
+#operadores logicos 
+
+print(3>4 and 4>5)
+print(3<4 and 4<5)
+print(3>4 or 4>5)
+print(3<4 or 4<5)
+print(not(3>4))
+
