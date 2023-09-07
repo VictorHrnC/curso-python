e0b7646c0336ccb28209b1d63336dfdfae8e181d
victor hernandez <victorherdev@gmail.com>
2023-09-07 09:17:53 -0300

    actualizo el fichero

--- strings.py
+++ strings.py
@@ -4,14 +4,30 @@
 #definimos strings con variables asi es mas facil de llamarlas dentro de nuestro programa
 er_str = "este es mi string"
 do__str = "este es mi segundo string"
-
 print(er_str)
+
 #podemos utilizar operadores como vimos en operadores.py line 10
 print(er_str + " " + do__str)
+
 #existen strings con caracteres especiales que provocan ciertas funciones
 #definamos un salto de linea entre el string
 str_con_salto = "este es un string\ncon salto de linea"
 print(str_con_salto)
+
 #definamos un tab en nuestro string
 str_con_tab = "\teste es un string con tabulacion" 
 print(str_con_tab)
+
+#al poner \\ en nuestos caracteres especiales vistos antes los anulamos
+str_escapados = "\\t este string no tiene tabulacion \\n ni salto de linea"
+print(str_escapados)
+
+#formateo de un string con %. y {}
+name, surname, age = "victor", "hernandez", 25
+#con {}
+print("mi nombre es {} {} y mi edad es {}".format(name,surname,age))
+#con %
+print("mi nombre es %s %s y mi edad es %d"%(name, surname, age))
+#con{y nombres dentro}
+print(f"mi nombre es {name} {surname} y mi edad es {age}")
+
