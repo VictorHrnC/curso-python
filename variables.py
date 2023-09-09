6b5e18130f12a95bf7a769f625ead0c5b2485b1e
victor hernandez <victorherdev@gmail.com>
2023-09-09 09:35:11 -0300

    actualización del fichero

--- variables.py
+++ variables.py
@@ -5,9 +5,13 @@
 str_var = "soy una variable de tipo string"
 print(str_var)
 
+#variable de tipo int
+
 int_var = 101100
 print(int_var)
 
+#cambiar una variable de int a string
 int_to_str = str(int_var)
 print(int_to_str)
 print(type(int_to_str))
+
