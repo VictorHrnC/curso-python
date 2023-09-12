9f02f5418a45ddd4401a721e2dd181461d8f5a64
victor hernandez <victorherdev@gmail.com>
2023-09-12 09:57:07 -0300

    nuevo fichero condicional (con un ejemplo login básico *vemos variables, listas operadores y el uso del if,elif*)

---
+++ condicionales.py
@@ -0,0 +1,38 @@
+### CONDICIONALES  ###
+
+#los condicionales como dice el mismo titulo son un tipo de validacion o funcion que nos permite hacer algo bajo ciertas condiciones
+#nos permite hacer una validacion de datos (en caso de un login)
+
+#veremos ejemplos sencillos
+#cambiar los valores para ver resultados
+
+condicion = True #false
+
+if condicion: #no necesitamos poner == true el programa asume que para pasar a la linea que esta abajo nuestra condiion debe ser verdadera (en caso que estemos trabajando con valores booleanos)
+    print("se ejecuta la condicion del if")
+
+print("seguimos programando")
+
+#veamos el caso en de un login
+#interactuamos con la terminal
+print("ingresa tu nombre:")
+nombre=input()
+print("ingresa una contraseña")
+contraseña=input()
+user=[nombre,contraseña]
+cadena_saltos=print("*");print("*");print("*");print("*");print("*");print("*");print("*");print("*");print("*");print("*");print("*");print("*");print("*********************************************************************************")
+cadena_saltos
+print("entrando al login")
+print("ingrese su nombre")
+nombre_comprovacion = input()
+print("ingresa una contraseña")
+contraseña_comprovacion=input()
+user_comprovacion=[nombre_comprovacion,contraseña_comprovacion]
+if user[0] == user_comprovacion[0] and user[1] == user_comprovacion[1]:
+    print("ingreso exitoso")
+elif user[0] == user_comprovacion[0] and user[1] != user_comprovacion[1]:
+    print("clave invalida")
+elif user[0] != user_comprovacion[0] and user[1] == user_comprovacion[1]:
+    print("usuario no valido")
+
+print("seguimos programando...")
