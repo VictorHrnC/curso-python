4027b2d0a91a7484ef7df61fbb8d930aa4464069
victor hernandez <victorherdev@gmail.com>
2023-09-14 12:57:51 -0300

    fichero hello

--- 01_hello.py
+++ 01_hello.py
@@ -1,4 +1,4 @@
-print("hello python")
+print("hello python");                                                                                                     """EN CASO DE ESTAR EN VSCODE EL EJECUTAR ESTA AQUI 🡹 """
 
 #este es un comentario
 ##este es otro comentario
@@ -8,5 +8,12 @@ este es
 un comentario
 de mas lineas
 """
-numero=15
-print(numero)
+
+###
+# hola este curso lo imparte @Mouredev solo que esta es una version escrita por comentarios
+# esta pensado para la introduccion a este lenguaje el cual es top en tendencias dentro de nuestro rubro
+# el curso esta pensado para personas que apesar de estar comensando en este lenguaje tienen nociones de programacion (se les hara mucho mas sencillo por los terminos que se utilizan)
+# conocen algunos tipos de datos, conocen algun otro lenguaje (haran que la comprencion de las sintaxys sea mas optima)
+# sin mas dilacion leamos nuestro curso###
+
+"""utilizamos print durante el curso para asi mostrar resultados en pantalla recordar hacer un execute desde el editor de texto que estes viendo este curso"""
