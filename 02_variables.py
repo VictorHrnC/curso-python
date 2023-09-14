8eea2a84003bd86c6b6a86260e83b8d097ee6c6f
victor hernandez <victorherdev@gmail.com>
2023-09-14 12:58:00 -0300

    fichero variables

--- 02_variables.py
+++ 02_variables.py
@@ -6,6 +6,7 @@ str_var = "soy una variable de tipo string"
 print(str_var)
 
 #variable de tipo int
+#estas variables nos pueden servir para utilizar operadores (lo veremos mas adelante :D)
 
 int_var = 101100
 print(int_var)
@@ -15,3 +16,4 @@ int_to_str = str(int_var)
 print(int_to_str)
 print(type(int_to_str))
 
+#recordar que podemos definir de esta forma todo tipo de datos enteros, float, string booleanos
