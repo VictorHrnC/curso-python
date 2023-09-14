ef7a63cfb55f9e3572aa77e207031d568dd9a762
victor hernandez <victorherdev@gmail.com>
2023-09-14 08:47:03 -0300

    fichero de loops (bucles o ciclos)

--- 07_loops.py
+++ 07_loops.py
@@ -18,3 +18,16 @@ while condicion <=100:
 else: #los else son opcionales nos serviria para mostrar un mensaje que salimos de nuestro bucle
     print("salimos del loop while")
 print("el valor de tu condicion es: " + str(condicion))
+
+
+# FOR
+
+#con el loop del for podemos listar un conunto de elementos 
+#definiendo una de las listas que se vieron anteriormente 
+
+lista = [25,35,18,25,14,24]
+
+#podemos cin un iterador x recorrerla facilmente nombrando cada uno de los elementos sin necesidad de acceder con lo que seria lista[0] , lista[1] etc
+
+for x in lista:
+    print(x)
