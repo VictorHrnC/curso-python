d12390e53c541795bef40467745394e55ab73a03
victor_hrnz <victorherdev@gmail.com>
2023-09-20 10:29:18 -0300

    fichero de clases

--- 09_clases.py
+++ 09_clases.py
@@ -17,6 +17,10 @@
 # nombres tan largos para que al llamarlos no sea tan engorroso
 
 
+from enum import Enum
+import keyboard
+
+
 class Persona():
     def __init__(self, name, surname, age):
         self.name = name
@@ -62,3 +66,108 @@ class Person():
 person = Person("solange", "hernandez", 35)
 print(person.fullname, person.age)
 person.caminar()
+
+# creacion de una clase con un elemento vacio o asignado
+# lo que hacemos es = el valor de uno de los elementos de nuestro avion puede ser de algun valor predeterminado o vacio
+# de esta forma en caso de que creemos un avio de tipo avion esto no nos dara un error
+
+
+class Avion():
+    # probar eliminar el ="" de tamaño para ver como en el objeto avion_1 se cae el problema
+    def __init__(self, tipo, motor, capacidad, tamaño=""):
+        self.tipo = tipo
+        self.motor = motor
+        self.capacidad = capacidad
+        self.tamaño = tamaño
+        self.descripcion = f"{tipo} {motor} {capacidad} {tamaño}"
+
+
+avion_1 = Avion("airbus319", "turbofan", 155,)
+
+avion_2 = Avion("airbus321", "turbofan", 210, "45m")
+
+print("se a creado un avion "+avion_1.descripcion)
+print("se a creado un avion "+avion_2.descripcion)
+
+""" hagamoslo dinamico
+    haremos una clase OtherPerson() y veamos que podemos hacer con ella
+    revisar lineas de codigo cuidadosamente hare comentarios para que vean como es que se esta trabajando para que no se pierdan
+    debemos de instalar el teclado con el pip desde nuestra bash de git con el comando pip install keyboard en caso de no haber instalado el pip desde un comienzo debemos de 
+    agregarlo con una modificacion a nuestro python (seria como reinstalar pero le damos a modificar)
+    recordad agregar los packages de librerias ahi estan las librerias para importar el keyboard
+"""
+
+# definimos nuestra clase OtherPerson
+
+
+class OtherPerson():
+    def __init__(self, name="", surname="", age=""):
+        self.fullname = f"{name} {surname}"
+        self.name = name
+        self.surname = name
+        self.age = age
+# le damos funciones que esta OtherPerson puede realizar
+
+    def caminar(self):
+        print(f"{self.fullname} esta caminando")
+
+    def saltar(self):
+        print(f"{self.fullname} esta saltando")
+
+    def correr(self):
+        print(f"{self.fullname} esta corriendo")
+
+# haremos una clase con una funcion para elegir que queremos hacer con nuestra persona
+# recordar que python no tiene la funcion de enum en el core por eso lo debemos de importar
+# por el formato de python los imports se fueron al principio
+
+
+class selec(Enum):
+    caminar = 1
+    saltar = 2
+    corer = 3
+
+# haremos una funcion para acceder a nuestra eleccion con un parametro que sera de clase selec (esta es una clase que viene creada dentro de el core de python)
+
+
+def eleccion(eleccion: selec):
+    match eleccion:
+        case selec.caminar:
+            OtherPerson.caminar()
+        case selec.saltar:
+            OtherPerson.saltar()
+        case selec.corer:
+            OtherPerson.correr()
+
+
+# definimos una lista persona para almacenar los valores que seran entregados a nuestro objeto my_person
+# creamos variables para insertar dentro de la lista, llamamos a la popiedad insert()
+persona = []
+print("ingrese un nombre")
+nombre = input()
+persona.insert(0, nombre)
+print("ingrese un apellido")
+apellido = input()
+persona.insert(1, apellido)
+print("ingrese la edad")
+edad = input()
+persona.insert(2, edad)
+# en este lugar haremos que nuestra lista persona con los valores que me des desde la terminal pasen a nuestro objeto que es de tipo OtherPerson
+my_person = OtherPerson(persona[0], persona[1], persona[2])
+# haremos nuestros prints para interactuar en la terminal
+print("preciona w para caminar")
+print("preciona *espacio* para saltar")
+print("preciona *tab* para correr")
+print("preciona *esc* para salir")
+# con un while de tipo true para que siempre se mantenga iniciada la interaccion
+while (True):
+    event = keyboard.read_event()
+    if event.name == "esc":
+        break
+    elif event.event_type == keyboard.KEY_DOWN:
+        if event.name == "w":
+            my_person.caminar()
+        elif event.name == "tab":
+            my_person.correr()
+        elif event.name == "space":
+            my_person.saltar()
